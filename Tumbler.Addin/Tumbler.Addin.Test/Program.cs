6566aaa918d74227c8a743e7a6bf50093ed24366
PC201608221249\Administrator <935714690@qq.com>
2017-04-05 20:33:20 +0800

    测试程序

--- Tumbler.Addin/Tumbler.Addin.Test/Program.cs
+++ Tumbler.Addin/Tumbler.Addin.Test/Program.cs
@@ -14,30 +14,32 @@ namespace Tumbler.Addin.Test
         {
             AddinManager manager = new AddinManager(_host, "addins.xml");
             AddinProxy[] proxies = manager.LoadAddins("menu").ToArray();
-            String[] a = { "0FAEE6AA-72BA-4E13-8689-2B1F86A2502C", "74D56627-BD68-4C0F-B006-AB643E72DB8B","" };
-            Message message = _host.CreateMulticastMessage(a, ContentType.JSON, new Byte[10]);
-            _host.Send(message);
-            manager.Unload(proxies[0]);
-            _host.Send(message);
-            Console.ReadKey();
 
-            message = _host.CreateUnicastMessage(proxies[1].Id, ContentType.JSON, new Byte[5]);
-            _host.Send(message);
-            Console.ReadKey();
-
-            //Message message = _host.CreateMessageToAll(false, ContentType.JSON, new Byte[2]);
+            //String[] a = { "0FAEE6AA-72BA-4E13-8689-2B1F86A2502C", "74D56627-BD68-4C0F-B006-AB643E72DB8B","" };
+            //Message message = _host.CreateMulticastMessage(a, ContentType.JSON, new Byte[10]);
+            //_host.Send(message);
+            //manager.Unload(proxies[0]);
             //_host.Send(message);
             //Console.ReadKey();
 
-            //_host.MessageService.Unregister(proxyB.Id);
-            //message = _host.CreateMessageToAll(false, ContentType.JSON, new Byte[2]);
+            //message = _host.CreateUnicastMessage(proxies[1].Id, ContentType.JSON, new Byte[5]);
             //_host.Send(message);
             //Console.ReadKey();
 
-            //Console.Clear();
-            //_host.MessageService.Register(proxyB);
-            //message = _host.CreateMessageToAll(false, ContentType.JSON, new Byte[2]);
-            //_host.Send(message);
+            Message message = _host.CreateBroadcastMessage(ContentType.JSON, new Byte[2]);
+            _host.Send(message);
+            Console.ReadKey();
+
+            Console.Clear();
+            _host.MessageService.Unregister(proxies[0].Id);
+            message = _host.CreateBroadcastMessage( ContentType.JSON, new Byte[2]);
+            _host.Send(message);
+            Console.ReadKey();
+
+            Console.Clear();
+            _host.MessageService.Register(proxies[0]);
+            message = _host.CreateBroadcastMessage(ContentType.JSON, new Byte[2]);
+            _host.Send(message);
             Console.ReadKey();
 
         }
